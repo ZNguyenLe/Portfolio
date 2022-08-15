7db619eaafc78e53f02a13ad6133c08fa1911d88
ZNguyenLe <90646702+ZNguyenLe@users.noreply.github.com>
2022-08-15 17:20:28 -0400

    updated contact section (WIP)

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+function show() {
+    let button = document.getElementById('contactInfo');
+    if (button.style.display === 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
+
